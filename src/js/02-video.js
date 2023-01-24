df11e4602998de3a21b66c3cdd2c990457926e0f
Vladislav Virchenko <virchenko.vlad.2011@icloud.com>
2023-01-24 18:22:03 +0200

    1

---
+++ src/js/02-video.js
@@ -0,0 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
+const currentTime = 'videoplayer-current-time';
+
+function onPlay(time) {
+    localStorage.setItem(currentTime, time.seconds);
+}
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(localStorage.getItem(currentTime))
+
